cfef9f2870aaa5645822243f4c3b3e399b4d4f93
Valentin <valentin@oncrawl.com>
2020-09-21 12:57:38 +0200

    fix: receiving dir

--- start_ipython.sh
+++ start_ipython.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 gcloud auth activate-service-account --key-file $GOOGLE_APPLICATION_CREDENTIALS
-gsutil -m cp -r gs://$GCP_PROJECT_IPYTHON/ipython/ ipython
+gsutil -m cp -r gs://$GCP_PROJECT_IPYTHON/ipython/ .
 
 python main.py $@
 gsutil -m cp -r ipython gs://$GCP_PROJECT_IPYTHON/ipython
